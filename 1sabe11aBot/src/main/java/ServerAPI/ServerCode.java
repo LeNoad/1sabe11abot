ad73d831f3cc555aaaae7ceb3aaf87fc86ea6808
1sabe11a <71987963+LeNoad@users.noreply.github.com>
2021-05-25 23:18:12 +0900

    Delete 1sabe11aBot directory

--- 1sabe11aBot/src/main/java/ServerAPI/ServerCode.java
+++
@@ -1,8 +0,0 @@
-package ServerAPI;
-
-public class ServerCode {
-    String[] server = {"cain","diregie","siroco","prey","casillas","hilder","anton","bakal"};
-    public String selectServer(int select){
-    	return server[select];
-    }
-}
--- 1sabe11aBot/src/main/java/ServerAPI/apiKey.java
+++
@@ -1,8 +0,0 @@
-package ServerAPI;
-
-public class apiKey {
-    private String apiKey = ""; //디스코드 API 토큰값
-    public String setapiKey() {
-        return apiKey;
-    }
-}
